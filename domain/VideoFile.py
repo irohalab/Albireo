d4b498502915b5a185dbbf119760ded4ab17b066
Everett Summer <55684557+EverettSummer@users.noreply.github.com>
2022-03-12 06:18:16 -0800

    Change type of some columns of table video_file

--- domain/VideoFile.py
+++ domain/VideoFile.py
@@ -1,5 +1,5 @@
 from domain.base import Base
-from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy import Column, Integer, String, ForeignKey, TEXT
 from sqlalchemy.dialects import postgresql
 from sqlalchemy.orm import relationship
 from uuid import uuid4
@@ -12,8 +12,8 @@ class VideoFile(Base):
     bangumi_id = Column(postgresql.UUID(as_uuid=True), ForeignKey('bangumi.id'), nullable=False)
     episode_id = Column(postgresql.UUID(as_uuid=True), ForeignKey('episodes.id'), nullable=False)
 
-    file_name = Column(String, nullable=True)
-    file_path = Column(String, nullable=True)  # file path is real path to the file in filesystem
+    file_name = Column(TEXT, nullable=True)
+    file_path = Column(TEXT, nullable=True)  # file path is real path to the file in filesystem
     # deprecated
     torrent_id = Column(String, nullable=True)  # torrent_id is deluge torrent id associated with this file, for imported file, torrent_id should be null
 
@@ -24,7 +24,7 @@ class VideoFile(Base):
     resolution_w = Column(Integer, nullable=True)
     resolution_h = Column(Integer, nullable=True)
     duration = Column(Integer, nullable=True)  # unit is millisecond
-    label = Column(String, nullable=True)  # label can be set by admin
+    label = Column(TEXT, nullable=True)  # label can be set by admin
 
     episode = relationship('Episode', back_populates='video_files')
     bangumi = relationship('Bangumi', back_populates='video_files')
